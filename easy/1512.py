e38fe0924751949b942f6ef0ea44f1a0ce0f93a2
londrwus <ssd7788@mail.ru>
2024-02-24 21:17:01 +0300

    easy solve

---
+++ easy/1512.py
@@ -0,0 +1,21 @@
+from typing import List
+
+nums = [1, 2, 3, 1, 1, 3]
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        cnt = 0
+        d = {}
+        for num in nums:
+            if num in d:
+                cnt += d[num]
+                d[num] += 1
+            else:
+                d[num] = 1
+
+        return cnt
+
+
+result = Solution().numIdenticalPairs(nums=nums)
+print(result)
